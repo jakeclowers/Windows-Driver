948dfceb0a9fcdd9935d192e0af50b5cb0d2f182
jakeclowers <133717847+jakeclowers@users.noreply.github.com>
2023-07-02 17:35:08 +0200

    Add files via upload

---
+++ Sample driver/GuidedHacking/GuidedHacking/guidedhacking.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <ntifs.h>
+
+
+
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath);
+
+NTSTATUS UnloadDriver(PDRIVER_OBJECT pDriverObject);
